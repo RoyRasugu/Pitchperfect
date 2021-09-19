a085841b1af1377cabab90919d9aa0a84bff4e78
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-20 02:04:42 +0300

    Import the flask_sqlalchemy

---
+++ config.py
@@ -0,0 +1,35 @@
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://royrasugu:12345@localhost/pitches'
+
+class ProdConfig(Config):
+    # '''
+    # Production configuration child class
+
+    # Args:
+    #     Config: The parent configurattion class with General configuration settings
+    # '''
+    pass
+
+
+class DevConfig(Config):
+    # '''
+    # Development configuration child class
+
+    # Args:
+    #     Config: The parent configuration class with General configuration settings
+    # '''
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
