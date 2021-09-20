48e8e7646ac01a141cbb9ace9dba5c18f7bada85
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-20 21:16:57 +0300

    use security feature for passwords

--- app/models.py
+++ app/models.py
@@ -1,11 +1,24 @@
-import re
+# import re
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_Secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_Secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_Secure, password)
 
     def __repr__(self):
         return f'User {self.username}'
