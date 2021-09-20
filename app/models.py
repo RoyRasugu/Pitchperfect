e24ae65445d3621ab5590352307832729633cbe3
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-20 23:48:43 +0300

    add bio and profile_pic_path columns

--- app/models.py
+++ app/models.py
@@ -2,11 +2,14 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_Secure = db.Column(db.String(255))
 
