41081f7ad7595e73736a24476a94104dbb231dee
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-21 07:17:03 +0300

    create mail message after one registers

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from . import auth
 from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import RegistrationForm,LoginForm
+from ..email import mail_message
 from .. import db
 
 @auth.route('/login',methods = ['GET','POST'])
@@ -32,7 +33,10 @@ def register():
         user = User(email = form.email.data, username = form.username.data, password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to AllInPitches","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
-    title = "New Account"
+        title = "New Account"
 
     return render_template('auth/register.html',registration_form = form)
